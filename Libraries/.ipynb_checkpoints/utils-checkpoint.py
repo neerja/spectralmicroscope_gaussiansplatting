4d2ccfb232aa29fa2a31ed3b38a85907554a4f87
rinnibhan <rinbha@stanford.edu>
2024-11-27 11:32:52 -0800

    Untracked the files I added to .gitignore

--- Libraries/.ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,8 +0,0 @@
-# gausssplat.py - library of functions for gaussian splatting for spectral diffuserscope
-# Neerja Aggarwal
-# Date created: Jan 2nd, 2024 
-# Last updated: Jan 2nd, 2024
-import torch
-import numpy as np
-import matplotlib.pyplot as plt
-from torch.distributions.multivariate_normal import MultivariateNormal
